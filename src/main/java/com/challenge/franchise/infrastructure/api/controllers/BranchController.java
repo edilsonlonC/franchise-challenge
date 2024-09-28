ca442304a4efd1414e979f7810290c66e07b79a3
edilsonlonc <eddylson.londonio@gmail.com>
2024-09-28 01:49:27 -0500

    update data from branch and franchise

--- src/main/java/com/challenge/franchise/infrastructure/api/controllers/BranchController.java
+++ src/main/java/com/challenge/franchise/infrastructure/api/controllers/BranchController.java
@@ -6,14 +6,12 @@ import com.challenge.franchise.domain.models.BranchModel;
 import com.challenge.franchise.domain.models.FranchiseModel;
 import com.challenge.franchise.infrastructure.api.dto.BranchCreateDto;
 import com.challenge.franchise.infrastructure.api.dto.BranchResponseDto;
+import com.challenge.franchise.infrastructure.api.dto.BranchUpdateDto;
 import com.challenge.franchise.infrastructure.mapper.BranchDtoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
@@ -36,5 +34,17 @@ public class BranchController {
                 branchUseCase.createBranch(branchModel)
         ), HttpStatus.CREATED);
 
+    }
+    @PutMapping
+    public ResponseEntity<BranchResponseDto> updateBranch(@RequestBody BranchUpdateDto branchUpdateDto) {
+        Optional<BranchModel> branchModelResult = branchUseCase.findById(branchUpdateDto.getId());
+        if(branchModelResult.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        BranchModel branchModel = branchModelResult.get();
+        branchModel.setName(branchUpdateDto.getName());
+        return new ResponseEntity<>(
+                branchDtoMapper.branchModelToBranchDtoResponse(
+                       branchUseCase.createBranch(branchModel)
+                ), HttpStatus.OK
+        );
     }
 }
--- src/main/java/com/challenge/franchise/infrastructure/api/controllers/FranchiseController.java
+++ src/main/java/com/challenge/franchise/infrastructure/api/controllers/FranchiseController.java
@@ -4,14 +4,15 @@ import com.challenge.franchise.application.useCases.FranchiseUseCase;
 import com.challenge.franchise.domain.models.FranchiseModel;
 import com.challenge.franchise.infrastructure.api.dto.FranchiseCreateDto;
 import com.challenge.franchise.infrastructure.api.dto.FranchiseResponseDto;
+import com.challenge.franchise.infrastructure.api.dto.FranchiseUpdateDto;
 import com.challenge.franchise.infrastructure.mapper.FranchiseDtoMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +27,18 @@ public class FranchiseController {
                 franchiseUseCase.createFranchise(franchiseModel)
         );
         return new ResponseEntity<>(franchiseResponseDto, HttpStatus.CREATED);
+    }
+    @PutMapping
+    public ResponseEntity<FranchiseResponseDto> update(@RequestBody FranchiseUpdateDto franchiseUpdateDto) {
+        Optional<FranchiseModel> franchiseModelResult = franchiseUseCase.findById(franchiseUpdateDto.getId());
+        if(franchiseModelResult.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        FranchiseModel franchiseModel = franchiseModelResult.get();
+        franchiseModel.setName(franchiseUpdateDto.getName());
+        return new ResponseEntity<>(
+                franchiseDtoMapper.franchiseModelToFranchiseResponseDto(
+                        franchiseUseCase.createFranchise(franchiseModel)
+                ), HttpStatus.OK
+        );
+
     }
 }
